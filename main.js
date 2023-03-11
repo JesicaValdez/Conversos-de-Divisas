a22920c7c6b08a9f2deebb9c41c9aca56d8f70fd
JesicaValdez <jesica.valddez@gmail.com>
2023-03-11 14:37:28 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,41 @@
+let = convert = document.getElementById("convert");
+let = cantidad = document.getElementById("cantidad");
+let = divisa = document.getElementById("divisa");
+let resultado = document.getElementById ("resultado");
+let modo = document.getElementById("modo");
+let contenedor = document.getElementById("contenedor");
+
+let pic = document.getElementById("pic");
+
+divisa.addEventListener("change", (e) => {
+    console.log(e.target.value);
+});
+
+modo.addEventListener("change", () => {
+    if(modo.checked) {
+        contenedor.classList.add("noche");
+    }else{
+        contenedor.classList.remove("noche");
+    }
+});
+
+let convertilos = () => {
+    let valor = 0;
+    let moneda = "usd";
+    if(divisa.value == "usd") {
+        valor = 370;
+        moneda = "usd";
+        resultado.style.color ="green";
+        pic.src = "https://upload.wikimedia.org/wikipedia/commons/f/fa/Presidential_dollar_coin_reverse.png"
+    } else {
+        valor = 380;
+        moneda = "eur";
+        resultado.style.color ="blue";
+        pic.src= "https://upload.wikimedia.org/wikipedia/commons/5/56/Reverso_1_euro.jpg"
+    }
+    let elegida = parseInt();
+    let cuanto = parseInt(cantidad.value);
+    let result = cuanto * valor;
+    resultado.innerHTML = cuanto + " " + moneda + " son: $" + result;
+};
+convert.addEventListener("click", convertilos);
